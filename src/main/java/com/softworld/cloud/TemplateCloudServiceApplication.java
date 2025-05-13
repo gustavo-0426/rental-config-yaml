e645d94a98548bebf52ed0dcc107a4ade1b967a2
Gustavo Castro <104918969+gustavo-0426@users.noreply.github.com>
2025-05-13 07:29:22 -0500

    Initial commit

---
+++ src/main/java/com/softworld/cloud/TemplateCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.softworld.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateCloudServiceApplication.class, args);
+	}
+
+}
